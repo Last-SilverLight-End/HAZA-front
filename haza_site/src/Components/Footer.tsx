09227911a68286afbfc7174a5585fa68637c19d9
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-01-28 09:05:53 +0900

    feat : footer 생성 및 링크 변경

--- haza_site/src/Components/Footer.tsx
+++ haza_site/src/Components/Footer.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
+import style from '@styles/footer.module.css';
 
 
-function Footer() {
+
+const Footer : React.FC = () =>{
+
+  
+    const getTodayDate = () =>{
+        const getNow = new Date();
+        const getMonth = getNow.getMonth()+1;
+        const getYear = getNow.getFullYear();
+
+
+    const getDate = getNow.getDate();
+
+    return [getYear,getMonth,getDate].join('.');
+  }
+
   return (
-    <div className="App">
-        Footer 입니다.
-    </div>
+    <footer className={style.footer}>
+      <h1>Footer 입니다.</h1>
+      <div className={style.div}>
+      <ul>
+        <li><a href='#'/></li>
+        <li><a href='#'/> 문의사항 </li> 
+      </ul>
+      </div>
+        <div className={style.copyright}>
+        ⓒ {getTodayDate()} HAZA , ALL RIGHTS RESERVED
+        </div>
+    </footer>
   );
 }
-
-
 
 export default Footer;
