477b896482eac598301d4ce8dc7b24ba64acb406
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-01-28 09:24:30 +0900

    feat : footer 생성 및 router 추가

--- haza_site/src/Routes/router.tsx
+++ haza_site/src/Routes/router.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../Components/Home';
+
 import {Community} from '../Components/Community';
-import {Survey} from '../Components/Survey';
+import {Survey} from "../Components/Survey";
+import Help from '../Help/Help';
 const router = createBrowserRouter([
   // 메인 홈페이지 관련 라우터
   {
@@ -32,6 +34,19 @@ const router = createBrowserRouter([
     path: "/Survey",
     element: <Survey/>,
   },
+  /* footer 이용약관 및 관련 사이트들 */
+  {
+    path: "/help",
+    element: <Help/>,
+    children: [
+      {
+        // 이용약관
+      },
+      {
+        //다른 사이트 이동
+      },
+    ]
+  }
 ]);
 
 export default router;
