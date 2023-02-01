7afcb3f5101b41c52f869f50a27df17dc216e726
Bellevue <alyac1234@gmail.com>
2023-02-02 01:52:51 +0900

    Feature-WIP: Backend Request (prototype)

--- haza_next/src/libs/oAuth.ts
+++ haza_next/src/libs/oAuth.ts
@@ -1,3 +1,7 @@
+/**
+ * oAuth를 위한 유틸리티 함수들
+ */
+
 import { BACKEND_TEST } from "./constants"
 
 const oAuthURIPrefix = `${BACKEND_TEST}/api/auth/login/`
@@ -8,6 +12,18 @@ export enum OAuthProvider {
   KAKAO = "kakao",
 }
 
+export interface TokenProp {
+  token: string
+}
+
 export function oAuthURI(provider: OAuthProvider) {
   return `${oAuthURIPrefix}${provider}`
+}
+
+export function getJWTToken() {
+  return localStorage.getItem("token") ?? ""
+}
+
+export function setJWTToken(token: string) {
+  localStorage.setItem("token", token)
 }
