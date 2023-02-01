becda410097828376ad31a26230c44ee8ed6b61b
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-02-02 00:19:40 +0900

    fix header

---
+++ haza_site/src/Help/Help.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Help() {
+  return (
+    <div className="App">
+        Help 통합 입니다.
+        <button href = "#"></button>
+        <button hre></button>
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
+  );
+}
+
+export default Help;
