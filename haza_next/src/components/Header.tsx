03d7b6db585d680186cb0572046b168a1e9db3e4
Bellevue <alyac1234@gmail.com>
2023-02-05 04:55:00 +0900

    Design: Switch dark mode button in header

--- haza_next/src/components/Header.tsx
+++ haza_next/src/components/Header.tsx
@@ -7,14 +7,20 @@ import tempImage from '../images/tempMain.jpg';
 import ShowLogin from './ShowLogin';
 import ShowStatus from './ShowStatus';
 
-import { Box, Button, ButtonGroup, Center, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Center, Flex, IconButton, Spacer, useColorMode } from "@chakra-ui/react";
 import { User } from './header/User';
 import { UserMenu } from './header/UserMenu';
 import { LoginMenu } from './header/LoginMenu';
 import Space from './Space';
+import { ChatIcon, EditIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 export function Header(props:Record<string, never>) {
   const router = useRouter();
+  // 상태들
+  // 로그인 체크용
+  const [login, setLogin] = useState<boolean>(false)
+  // 다크모드
+  const { colorMode, toggleColorMode } = useColorMode()
 
   // community 버튼 클릭
   const onCommunityClick = () => {
@@ -24,10 +30,6 @@ export function Header(props:Record<string, never>) {
   const onSurveyClick = () => {
     router.push("/survey/surveyHome");
   };
-
-
-  // check boolean , 로그인 체크용
-  const [login, setLogin] = useState<boolean>(false)
 
   // 렌더링
   return (
@@ -47,11 +49,13 @@ export function Header(props:Record<string, never>) {
           <Spacer />
           {/* 오른쪽 사이드 */}
           <Center>
-            <Flex columnGap={4}>
-              <Button colorScheme="cyan" onClick={onCommunityClick} className="ml-auto">대화 HAZA</Button>
-              <Button colorScheme="orange" onClick={onSurveyClick}>설문조사 HAZA</Button>
+            <Flex columnGap={2}>
+              <IconButton colorScheme="cyan" onClick={onCommunityClick} aria-label="대화 HAZA" icon={<ChatIcon />}/>
+              <IconButton colorScheme="orange" onClick={onCommunityClick} aria-label="설문조사 HAZA" icon={<EditIcon />}/>
+              {/* 다크모드 스위치 */}
+              <IconButton onClick={toggleColorMode} aria-label="Switch color mode" icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />} />
             </Flex>
-            <Spacer w={10} />
+            <Spacer w={8} />
             {/* 로그인 여부에 따라 바뀌는 컴포넌트*/}
             {login ?
               <UserMenu src="/images/profile_example.png" name="사람"
