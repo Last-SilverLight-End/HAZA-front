9b14b29bf476fdf115d2acec93919ffc89a71279
Bellevue <alyac1234@gmail.com>
2023-02-05 06:30:53 +0900

    Experimental: Basic Lexical Editor

---
+++ haza_next/src/components/lexical/Editor.tsx
@@ -0,0 +1,63 @@
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { CodeHighlightNode, CodeNode } from "@lexical/code";
+import { AutoLinkNode, LinkNode } from "@lexical/link";
+import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
+import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import { TRANSFORMERS } from "@lexical/markdown";
+
+function Placeholder() {
+  return <div className="editor-placeholder">Enter some rich text...</div>;
+}
+
+const editorConfig = {
+  // The editor theme
+  theme: {},
+  // Handling of errors during update
+  onError(error:unknown) {
+    throw error;
+  },
+  // Any custom nodes go here
+  nodes: [
+    HeadingNode,
+    ListNode,
+    ListItemNode,
+    QuoteNode,
+    CodeNode,
+    CodeHighlightNode,
+    TableNode,
+    TableCellNode,
+    TableRowNode,
+    AutoLinkNode,
+    LinkNode
+  ]
+};
+
+export default function Editor() {
+  return (
+    <LexicalComposer initialConfig={editorConfig as any}>
+      <div className="editor-container">
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={<ContentEditable className="editor-input" />}
+            placeholder={<Placeholder />}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+          <ListPlugin />
+          <LinkPlugin />
+          <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+        </div>
+      </div>
+    </LexicalComposer>
+  );
+}
--- haza_next/src/pages/Community/board.tsx
+++ haza_next/src/pages/Community/board.tsx
@@ -1,19 +1,25 @@
-import Footer from "@/components/Footer";
-import { Header } from "@/components/Header";
-import { TokenProp } from "@/libs/oAuth";
+import Footer from "@/components/Footer"
+import { Header } from "@/components/Header"
+import { TokenProp } from "@/libs/oAuth"
 
 import { Box, FormControl, FormLabel, FormHelperText, FormErrorMessage, Input, Center } from "@chakra-ui/react"
+import { useState } from "react"
+import dynamic from "next/dynamic"
+import Editor from "@/components/lexical/Editor"
 
 export default function Board(props: TokenProp) {
+  // 테스트
   return (
     <>
       <Header />
       <Center width="100%">
         <Box maxW="lg" p={4} borderWidth="2px">
           <FormControl>
-            <FormLabel>Email Address</FormLabel>
-            <Input type="email" />
+            <FormLabel>제목</FormLabel>
+            <Input type="text" />
             <FormHelperText>We will never share your email.</FormHelperText>
+            <FormLabel>내용</FormLabel>
+            <Editor />
           </FormControl>
       </Box>
       </Center>
