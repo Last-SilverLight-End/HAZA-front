2d3b8ad97fb848bf9c46eb20e4339d4cfd5897bf
Last-SilverLight-End <halfwing1998@gmail.com>
2023-02-12 17:41:51 +0900

    chore : change props

--- haza_next/src/components/generic/ContentFrame.tsx
+++ haza_next/src/components/generic/ContentFrame.tsx
@@ -1,7 +1,7 @@
 import { maxPageWidth } from "@/libs/constants";
-import { ChildrenProp } from "@/libs/util";
 import { Box, Center } from "@chakra-ui/react";
-
+import { ReactNode } from "react";
+import { ListBoardLine } from "../CommunityMainBoxContent";
 export type ContentFrameProps = {
   rowGap?: number | string;
 }
@@ -9,12 +9,23 @@ export type ContentFrameProps = {
 /**
  * 페이지 내용을 감싸는 프레임
  */
-export function ContentFrame({children, rowGap}: ChildrenProp & ContentFrameProps) {
+export function ContentFrame({ children, rowGap }: { 'children'?: ReactNode } & ContentFrameProps) {
   return (
     <Center width="100%">
       <Center width="100%" flexDirection="column" maxW={maxPageWidth} p={4} as="main" gap={rowGap} pt={10} pb={10}>
         {children}
       </Center>
     </Center>
+  )
+}
+
+/**
+ * 페이지 보드 리스트를 감싸는 프레임
+ */
+export function BoardFrame({ children }: { 'children'?: ReactNode }) {
+  return (
+    <Box flex='1'>
+      {children}
+    </Box>
   )
 }
